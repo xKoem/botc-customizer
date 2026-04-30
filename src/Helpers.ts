c7fe3a76289b1075fb5e2a89238d9aef403299fc
Koem <kosmi.bartek@gmail.com>
2026-04-30 19:35:59 +0200

    Add slider with description on how to use it

--- src/Helpers.ts
+++ src/Helpers.ts
@@ -4,7 +4,7 @@ export const mapFromSavedJsonToConfig = (json: any)=> {
     return templates.map(t => ({
         key: t.key,
         enabled: json.find((s: any) => s.key === t.key)?.enabled ?? true,
-        isNew: !json.some((s: any) => s.key === t.key),
+        isNew: json.length === 0 ? false : !json.some((s: any) => s.key === t.key),
         variables: Object.fromEntries(
             t.variables?.map(v => [
                 v.key,
